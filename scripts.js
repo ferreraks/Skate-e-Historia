098585fc2001a33b1d348d6902f8e22479fbc8a7
Arthur Ferreira Borges <115766497+ferreraks@users.noreply.github.com>
2024-09-09 01:03:38 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,19 @@
+// scripts.js
+
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita o envio automático
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (username === '' || password === '') {
+        alert('Por favor, preencha todos os campos.');
+    } else {
+        // Simulação de autenticação bem-sucedida
+        // Aqui você pode adicionar a lógica de autenticação real ou redirecionamento
+        // Por exemplo, enviar uma requisição ao backend para validar o login.
+
+        // Redirecionamento para o feed de atividades ou perfil do usuário
+        window.location.href = "/feed.html" // Substitua por 'perfil.html' ou outra página conforme necessário
+    }
+});
