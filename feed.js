242b26a7d2186ff574a22855ebdffc373f441c3a
Arthur Ferreira Borges <115766497+ferreraks@users.noreply.github.com>
2024-09-10 01:31:17 -0300

    agora sim

--- feed.js
+++ feed.js
@@ -16,3 +16,24 @@ newsletterForm.addEventListener('submit', (e) => {
     alert('Obrigado por se inscrever em nossa newsletter!');
     newsletterForm.reset();
 });
+
+// const gifElement = document.getElementById('gif1');
+// 
+// gifElement.addEventListener('mouseover', function() {
+//   gifElement.src = gifElement.getAttribute('data-gif1'); // Troca para o GIF
+// });
+// 
+// gifElement.addEventListener('mouseout', function() {
+//   gifElement.src = gifElement.getAttribute('data-static1'); // Volta para a imagem estática
+// });
+// 
+// const gifElement = document.getElementById('gif');
+// 
+// gifElement.addEventListener('mouseover', function() {
+//   gifElement.src = gifElement.getAttribute('data-gif2'); // Troca para o GIF
+// });
+// 
+// gifElement.addEventListener('mouseout', function() {
+//   gifElement.src = gifElement.getAttribute('data-static'); // Volta para a imagem estática
+// });
+
